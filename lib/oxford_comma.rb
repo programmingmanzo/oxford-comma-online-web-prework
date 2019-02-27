6d1574c252d1c1594b8da1e16080ea44ba80e952
Gerardo Manzo <programmingmanzo@gmail.com>
2019-02-27 09:46:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,7 +8,8 @@ def oxford_comma(array)
     last_element = array[2]
     array.pop 
     array << "and #{last_element}"
-    new_array = array 
+    new_array = []
+    new_array = array
     new_array.join
   else
     array.join
