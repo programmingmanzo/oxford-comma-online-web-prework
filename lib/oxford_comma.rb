a282b7a778b3b48e0fb5d63aa2fb0984b989d7d1
Gerardo Manzo <programmingmanzo@gmail.com>
2019-02-27 11:02:03 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+    elsif array.size == 2 
+    array.join(" and ")
+    elsif array.size == 3
+    last_element = "and #{array[-1]}"
+    array.pop 
+    array.push(last_element)
+    array.join(", ")
+  else
+    last_element = "and #{array[-1]}"
+    array.pop 
+    array.push(last_element)
+    array.join(", ")
+  end
 end
